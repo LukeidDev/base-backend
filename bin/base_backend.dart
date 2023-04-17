4dbaf6b614a2379259012c72d67acbde083137c7
LukeidDev <luccasdaniel92@gmail.com>
2023-04-17 17:36:31 -0300

    configurando banco de dados

--- bin/base_backend.dart
+++ bin/base_backend.dart
@@ -3,15 +3,27 @@ import 'dart:io';
 import 'package:base_backend/base_backend.dart' as base_backend;
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf/shelf.dart';
+import 'package:mysql1/mysql1.dart';
 
-void main(List<String> arguments) {
-  serve(
+void main(List<String> arguments) async {
+  final conn = await MySqlConnection.connect(
+    ConnectionSettings(
+      host: 'localhost',
+      port: 3306,
+      user: 'root',
+      db: 'delivery',
+      password: '1234',
+    ),
+  );
+  print(await conn.query('select * from tb_permissoes'));
+
+  await serve(
     (request) => Response(
       200,
       body: 'Ola Mundo',
       headers: {'content-type': 'application/json'},
     ),
     'localhost',
-    8080,
+    8082,
   );
 }
